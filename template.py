4ce9475edab42683ff76364a0d2c9eb77132e695
Rui Lu <33632389+lurui87@users.noreply.github.com>
2025-02-16 10:26:29 -0500

    set template of project

---
+++ template.py
@@ -0,0 +1,42 @@
+import os 
+from pathlib import Path 
+import logging 
+
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]:%(message)s:')
+
+project_name="datascience"
+list_of_files=[".github/workflow/.gitkeep",
+               f"src/{project_name}/_init_.py",
+               f"src/{project_name}/components/_init_.py",
+               f"src/{project_name}/utils/_init_.py",
+               f"src/{project_name}/utils/common.py",
+               f"src/{project_name}/config/_init_.py",
+               f"src/{project_name}/config/configuration.py",
+               f"src/{project_name}/pipeline/_init_.py",
+               f"src/{project_name}/entity/_init_.py",
+               f"src/{project_name}/entity/config_entity.py",
+               f"src/{project_name}/constants/_init_.py",
+               "config/congfig.yaml",
+               "params.yaml",
+               "schema.yaml",
+               "main.py" ,
+               "Dockerfile",
+               "setup.py",
+               "reserach/reserach.ipynb",
+               "templates/index.html"]
+
+for filepath in list_of_files:
+    filepath=Path(filepath)
+    filedir,filename=os.path.split(filepath)
+    if filedir!="":
+     os.makedirs(filedir,exist_ok=True)
+     logging.info(f"Creating directory{filedir} for the file :{filename}")
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath)==0):
+      with open (filepath, "w") as f:
+           pass 
+           logging.info(f"Creating empty file:{filepath}")
+    else :
+          logging.info(f"File {filename} aldready exists")
+       
+
+
